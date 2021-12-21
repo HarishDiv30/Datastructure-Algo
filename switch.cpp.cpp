6ef15f058b29027ce0cc2a9b91b9c0567d667a9f
Harishchandra Divase <86156756+HarishDiv30@users.noreply.github.com>
2021-12-21 19:21:38 +0530

    Switch case in C++
    Here is a basic code of the Switch case that is implemented

---
+++ switch.cpp.cpp
@@ -0,0 +1,25 @@
+//
+// Created by amazo on 12/21/2021.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char button;
+    cout<<"Enter a character:";
+    cin>>button;
+    if(button== 'a')
+    {
+        cout<<"Hello harish" << endl;
+    } else if (button=='b')
+    {
+        cout<<"Namaste Harry Bhau:"<<endl;
+    } else if(button=='c'){
+        cout<<"wadakum hari anna"<<endl;
+    } else if(button=='d'){
+        cout<<"Sasriya kaal ji Hari singh:"<< endl;
+    }
+    return 0;
+}
