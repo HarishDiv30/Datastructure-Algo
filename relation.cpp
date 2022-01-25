11a657695e36c6e6c7c9970d55f33c9f26a2a864
Harishchandra Divase <86156756+HarishDiv30@users.noreply.github.com>
2022-01-25 20:44:22 +0530

    Create relation.cpp
    Here I'm using relational operation

---
+++ relation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n>10){
+        cout<<"Value is more than 10"<<endl;
+    } else if(n<10){
+        cout<<"value is less than 10"<<endl;
+    } else{
+        cout<<"Value is equal to 10"<<endl;
+    }
+    return 0;
+}
